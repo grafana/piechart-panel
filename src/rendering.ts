bc6039c69ad1ac0b633c6ce6bacbc03940286554
Brian Gann <bkgann@gmail.com>
2019-07-21 22:23:47 -0500

    update to typescript

--- src/rendering.ts
+++ src/rendering.ts
@@ -1,105 +1,111 @@
 import _ from 'lodash';
+import './lib/jquery.flot.pie';
 import $ from 'jquery';
-import 'jquery.flot';
-import 'jquery.flot.pie';
+//import './lib/jquery.flot';
 
-export default function link(scope, elem, attrs, ctrl) {
-  var data;
-  var panel = ctrl.panel;
+export default function link(scope: any, elem: any, attrs: any, ctrl: any) {
+  let data;
+  const panel = ctrl.panel;
   elem = elem.find('.piechart-panel__chart');
-  var $tooltip = $('<div id="tooltip">');
+  const $tooltip = $('<div id="tooltip">') as any;
 
-  ctrl.events.on('render', function () {
+  ctrl.events.on('render', () => {
     if (panel.legendType === 'Right side') {
       render(false);
-      setTimeout(function () { render(true); }, 50);
+      setTimeout(() => {
+        render(true);
+      }, 50);
     } else {
       render(true);
     }
   });
 
-  function getLegendHeight(panelHeight) {
+  function getLegendHeight(panelHeight: any) {
     if (!ctrl.panel.legend.show || ctrl.panel.legendType === 'Right side' || ctrl.panel.legendType === 'On graph') {
       return 20;
     }
 
-    if (ctrl.panel.legendType == 'Under graph' && ctrl.panel.legend.percentage || ctrl.panel.legend.values) {
-      let breakPoint = parseInt(ctrl.panel.breakPoint) / 100;
-      var total = 23 + 20 * data.length;
+    if ((ctrl.panel.legendType === 'Under graph' && ctrl.panel.legend.percentage) || ctrl.panel.legend.values) {
+      const breakPoint = parseInt(ctrl.panel.breakPoint, 10) / 100;
+      const total = 23 + 20 * data.length;
       return Math.min(total, Math.floor(panelHeight * breakPoint));
     }
+
+    return 0;
   }
 
-  function formatter(label, slice) {
-    var slice_data = slice.data[0][slice.data[0].length - 1];
-    var decimal = 2;
-    var start = "<div style='font-size:" + ctrl.panel.fontSize + ";text-align:center;padding:2px;color:" + slice.color + ";'>" + label + "<br/>";
+  function formatter(label: any, slice: any) {
+    const sliceData = slice.data[0][slice.data[0].length - 1];
+    let decimal = 2;
+    const start = "<div style='font-size:" + ctrl.panel.fontSize + ';text-align:center;padding:2px;color:' + slice.color + ";'>" + label + '<br/>';
 
     if (ctrl.panel.legend.percentageDecimals) {
       decimal = ctrl.panel.legend.percentageDecimals;
     }
     if (ctrl.panel.legend.values && ctrl.panel.legend.percentage) {
-      return start + ctrl.formatValue(slice_data) + "<br/>" + slice.percent.toFixed(decimal) + "%</div>";
+      return start + ctrl.formatValue(sliceData) + '<br/>' + slice.percent.toFixed(decimal) + '%</div>';
     } else if (ctrl.panel.legend.values) {
-      return start + ctrl.formatValue(slice_data) + "</div>";
+      return start + ctrl.formatValue(sliceData) + '</div>';
     } else if (ctrl.panel.legend.percentage) {
-      return start + slice.percent.toFixed(decimal) + "%</div>";
+      return start + slice.percent.toFixed(decimal) + '%</div>';
     } else {
       return start + '</div>';
     }
   }
 
   function noDataPoints() {
-    var html = '<div class="datapoints-warning"><span class="small">No data points</span></div>';
+    const html = '<div class="datapoints-warning"><span class="small">No data points</span></div>';
     elem.html(html);
   }
 
   function addPieChart() {
-    var width = elem.width();
-    var height = ctrl.height - getLegendHeight(ctrl.height);
+    const width = elem.width();
+    const height = ctrl.height - getLegendHeight(ctrl.height);
 
-    var size = Math.min(width, height);
+    const size = Math.min(width, height);
 
-    var plotCanvas = $('<div></div>');
-    var plotCss = {
+    const plotCanvas = $('<div></div>');
+    const plotCss = {
       margin: 'auto',
       position: 'relative',
       paddingBottom: 20 + 'px',
-      height: size + 'px'
+      height: size + 'px',
     };
 
     plotCanvas.css(plotCss);
 
-    var backgroundColor = $('body').css('background-color')
+    const backgroundColor = $('body').css('background-color');
 
-    var options = {
+    const options = {
       legend: {
-        show: false
+        show: false,
       },
       series: {
         pie: {
+          radius: 1,
+          innerRadius: 0,
           show: true,
           stroke: {
             color: backgroundColor,
-            width: parseFloat(ctrl.panel.strokeWidth).toFixed(1)
+            width: parseFloat(ctrl.panel.strokeWidth).toFixed(1),
           },
           label: {
             show: ctrl.panel.legend.show && ctrl.panel.legendType === 'On graph',
-            formatter: formatter
+            formatter: formatter,
           },
           highlight: {
-            opacity: 0.0
+            opacity: 0.0,
           },
           combine: {
             threshold: ctrl.panel.combine.threshold,
-            label: ctrl.panel.combine.label
-          }
-        }
+            label: ctrl.panel.combine.label,
+          },
+        },
       },
       grid: {
         hoverable: true,
-        clickable: false
-      }
+        clickable: false,
+      },
     };
 
     if (panel.pieType === 'donut') {
@@ -109,7 +115,7 @@ export default function link(scope, elem, attrs, ctrl) {
     data = ctrl.data;
 
     for (let i = 0; i < data.length; i++) {
-      let series = data[i];
+      const series = data[i];
 
       // if hidden remove points
       if (ctrl.hiddenSeries[series.label]) {
@@ -117,17 +123,16 @@ export default function link(scope, elem, attrs, ctrl) {
       }
     }
 
-
     if (panel.legend.sort) {
       if (ctrl.panel.valueName !== panel.legend.sort) {
         panel.legend.sort = ctrl.panel.valueName;
       }
       if (panel.legend.sortDesc === true) {
-        data.sort(function (a, b) {
+        data.sort((a: any, b: any) => {
           return b.legendData - a.legendData;
         });
       } else {
-        data.sort(function (a, b) {
+        data.sort((a: any, b: any) => {
           return a.legendData - b.legendData;
         });
       }
@@ -135,40 +140,42 @@ export default function link(scope, elem, attrs, ctrl) {
 
     elem.html(plotCanvas);
 
+    // @ts-ignore
     $.plot(plotCanvas, data, options);
-    plotCanvas.bind("plothover", function (event, pos, item) {
+    plotCanvas.bind('plothover', (event: any, pos: any, item: any) => {
       if (!item) {
         $tooltip.detach();
         return;
       }
 
-      var body;
-      var percent = parseFloat(item.series.percent).toFixed(2);
-      var formatted = ctrl.formatValue(item.series.data[0][1]);
+      let body;
+      const percent = parseFloat(item.series.percent).toFixed(2);
+      const formatted = ctrl.formatValue(item.series.data[0][1]);
 
       body = '<div class="piechart-tooltip-small"><div class="piechart-tooltip-time">';
       body += '<div class="piechart-tooltip-value">' + _.escape(item.series.label) + ': ' + formatted;
-      body += " (" + percent + "%)" + '</div>';
-      body += "</div></div>";
+      body += ' (' + percent + '%)' + '</div>';
+      body += '</div></div>';
 
       $tooltip.html(body).place_tt(pos.pageX + 20, pos.pageY);
     });
   }
 
-  function render(incrementRenderCounter) {
-    if (!ctrl.data) { return; }
+  function render(incrementRenderCounter: any) {
+    if (!ctrl.data) {
+      return;
+    }
 
     data = ctrl.data;
 
-      if (0 == ctrl.data.length) {
-        noDataPoints();
-      } else {
-        addPieChart();
-      }
+    if (0 === ctrl.data.length) {
+      noDataPoints();
+    } else {
+      addPieChart();
+    }
 
     if (incrementRenderCounter) {
       ctrl.renderingCompleted();
     }
   }
 }
-
